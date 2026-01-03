75fe2901f45eff8621f6ee90edbd3b8581993333
Alec Sands <sandsalecv@gmail.com>
2026-01-03 13:57:45 -0800

    Add keybinding to copy diagnostic message to clipboard

--- .config/nvim/lua/opt.lua
+++ .config/nvim/lua/opt.lua
@@ -20,3 +20,34 @@ local colors = {
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = colors.light_gray })
 vim.api.nvim_set_hl(0, "StatusLine", { fg = colors.dark_gray, bg = colors.light_gray_2, bold = true })
 vim.api.nvim_set_hl(0, "StatusLineNC", { fg = colors.dark_gray, bg = colors.light_gray, bold = true })
+
+local yank_diagnostics = function()
+	local buff_name = vim.fn.bufname("")
+	local buff_numb = vim.fn.bufnr(buff_name)
+	local line_numb = vim.fn.line(".") - 1
+	local diagnostics = vim.diagnostic.get(buff_numb, { lnum = line_numb })
+	local val = ""
+	for _, diagnostic in ipairs(diagnostics) do
+		local severityName = ""
+		if diagnostic.severity == vim.diagnostic.severity.ERROR then
+			severityName = "Error"
+		elseif diagnostic.severity == vim.diagnostic.severity.WARN then
+			severityName = "Warning"
+		elseif diagnostic.severity == vim.diagnostic.severity.INFO then
+			severityName = "Info"
+		elseif diagnostic.severity == vim.diagnostic.severity.HINT then
+			severityName = "Hint"
+		end
+		val = string.format(
+			"%s%s: %s [%s] %s\n",
+			val,
+			severityName,
+			diagnostic.message,
+			diagnostic.code,
+			diagnostic.source
+		)
+	end
+	vim.fn.setreg("+", val)
+end
+
+vim.keymap.set("n", "<leader>dy", yank_diagnostics)
