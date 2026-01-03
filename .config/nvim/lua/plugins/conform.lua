45ccc3c91bcd3f6d31aa2f066176193e1abb782e
Alec Sands <sandsalecv@gmail.com>
2026-01-03 10:29:26 -0800

    Simplify and improve formatting decisions

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,11 +4,8 @@ return {
 	cmd = { "ConformInfo" },
 	opts = {
 		formatters_by_ft = {
-			javascript = { "prettierd" },
-			typescript = { "prettierd" },
-			javascriptreact = { "prettierd" },
-			typescriptreact = { "prettierd" },
 			lua = { "stylua" },
+			["_"] = { "prettierd" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
